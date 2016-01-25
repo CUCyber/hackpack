7cb57a2eeea41026e447b34e2e277fdf76413811
Robert Underwood <rr.underwood94@gmail.com>
2016-01-24 21:51:10 -0500

    More Ansible, and updated to 2.0 syntax
    - Added sections on documentation, templates, and ansible-vault
    - Updated syntax to use 2.0 style where necessary
    - Added an example extension like what might be used for site facts

--- tools/provisioners/ansible/scripts/ansible-install.sh
+++ tools/provisioners/ansible/scripts/ansible-install.sh
@@ -3,10 +3,18 @@ git clone git://github.com/ansible/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
 
+# For really broken machines such as Metaspoitable, install python27 from source
+# You may even have to disable certificate checking (-k) but don't do that if you
+# don't have to;  You will also need to set ansible_python_interpertor for these machines
+curl -LO https://www.python.org/ftp/python/2.7/Python-2.7.tgz
+tar -xzvf ./Python-2.7.tgz
+cd ./Python-2.7
+./configure && make && make install
+
 # or install from pip
 sudo pip install ansible
 
-# Install dependencies
+# Install dependencies if installing Ansible from github
 sudo pip install paramiko PyYAML Jinja2 httplib2 six
 
 # To manage Windows machines
